bf9718fcbac3f05060931dcb09684f94a9336259
Amulek1416 <zcarico03@gmail.com>
2019-06-07 16:53:22 -0600

    Changed the structure of the threads

--- flight.cpp
+++ flight.cpp
@@ -31,17 +31,18 @@
  * ******************************************/
 void init()
 {
-   // Structures for threading
-   sem_init(&dataSem, 0, 1);
-   sem_init(&logSem, 0, 1);
+   // Structures to initialize for threading
+   //    (CURRENTLY NONE)
+
+   // Flight variables
    inFlight = false;
-   startOfFlight = true;
+   startOfFlight = false;
 
    // Data Structures
-   data.resize(INIT_DATA_SIZE);
+   //    (CURRENTLY NONE)
 
    // Other Structures
-
+   //    (CURRENTLY NONE)
 }
 
 /********************************************
@@ -51,22 +52,32 @@ void init()
 void start()
 {
    init();
-   pthread_create(&dataID, NULL, getData, NULL);
-   pthread_create(&saveID, NULL, saveData, NULL);
-   pthread_create(&logID, NULL, logData, NULL);
+   pthread_create(&collectDataID, NULL, collectData, NULL);
+   pthread_create(&sendDataID, NULL, sendData, NULL);
+   int tempACC = *data.acc;
+   float tempALT = data.alt;
+   while((*data.acc - tempACC < ACC) && (data.alt - tempALT < ALT))
+   {
+     log("i", "Waiting for launch\n");
+     delayMicroseconds(10);
+   }
+   inFlight = true;
+   startOfFlight = true;
 }
 
 /********************************************
  * END: Joins the threads together, and calls
  *    any functions that may need to be called
- *    at the end of flight.
+ *    at the end of flight. Eventually the 
+ *    rocket will try to send out its GPS 
+ *    location here so we can easily find it.
  * ******************************************/
 void end()
 {
-   pthread_join(dataID, NULL);
-   pthread_join(saveID, NULL);
-   pthread_join(logID, NULL);
+   logData();
    saveSummary();
+   pthread_join(collectDataID, NULL);
+   pthread_join(sendDataID, NULL);
 }
 
 /********************************************
@@ -80,59 +91,55 @@ void saveSummary()
    //       data structure to a file.
 }
 
-///////////// CONCURRENT THREADS /////////////
+/********************************************
+ * LOG_DATA: This will save the logs in the 
+ *    queue to a file at the end of the flight.
+ * ******************************************/
+void logData()
+{
+   // TODO: Save log info to a log file
+}
+
+///////////// CONCURRENT FUNCTIONS /////////////
 
 /********************************************
  * GET_DATA: Function collects data from the 
- *    sensors and adds them to the deque.
+ *    sensors and saves them to a file. These
+ *    two steps aren't threaded, otherwise we
+ *    would eventually overflow memory.
  * ******************************************/
-void *getData(void* p)
-{
-   int buffer = 10;
-   struct FlightData *tData[buffer];
-   
-   for(int i = 0; i < buffer; i++)
-      tData[i] = NULL;
-   
+void *collectData(void* p)
+{  
    int fd = bmpSetup(); // Used for reading from sensors
-   int canWrite = 0;
-   int holder = 0;
-   int i = 0;
+   // TODO: Open file here
+   while(!inFlight)
+   {
+      data.alt = getAltitude(fd);
+      readACC(data.acc);
+   }
+
    while(inFlight)
    {
-      // Fill up tData with sensor data
-      do{
-         readACC(tData[holder]->acc);
-         readMAG(tData[holder]->mag);
-         readGYR(tData[holder]->gyr);
-         tData[holder]->alt = getAltitude(fd);
-         holder++;
+      readACC(data.acc);
+      readMAG(data.mag);
+      readGYR(data.gyr);
+      data.alt = getAltitude(fd);
+      
+      // TODO: Save data to file here
 
-         sem_getvalue(&dataSem, &canWrite);
-      }while(!canWrite && holder != buffer - 1);
-
-      // Store data between sem functions. The
-      //    sem functions are what make the 
-      //    program thread safe. We don't want
-      //    two threads editing the same info
-      //    at the same time.
-      sem_wait(&dataSem);
-      // TODO: Possibly make this a producer-consumer situation
-      for(i = 0; i < holder; i++)
-         data.push_front(*tData[i]);
-      sem_post(&dataSem);
    }
+   // TODO: Close file here
+
 }
 
 /********************************************
- * LOG_DATA: When there is something on the 
- *    queue, the logData function will wakeup,
- *    save logs to a file, then go back asleep
- *    when the queue is empty.
+ * SEND_DATA: Function sends data to the ground
+ *    durring flight.
  * ******************************************/
-void *logData(void* p)
+void *sendData(void *p)
 {
-   // TODO: Save log info to a log file
+   // This can be left empty until the 
+   // communications system is complete.
 }
 
 /********************************************
@@ -141,20 +148,12 @@ void *logData(void* p)
  *    the structure.
  * 
  *    TAGS: 
- *       (TODO: Create definitions for the tags)
+ *          "i" = "info"      "e" = "error"
+ *        "wtf" = "What a terrible failure"
  * ******************************************/
 void log(char* tag, char* message)
 {
-   // TODO: Write logic to push data onto "logs" structure
-}
-
-/********************************************
- * SAVE_DATA: Save the data being processed 
- *    by "getData". 
- * ******************************************/
-void *saveData(void *p)
-{
-   //TODO: Save data to a .csv file or something similar.
+   // TODO: Write logic to push data onto logs queue
 }
 
 /********************************************
@@ -164,7 +163,39 @@ void flightProgram()
 {
    start();
 
-   // TODO: Create flight logic here
+   // So we don't accidently deploy chutes too early
+   delayMicroseconds(MACH_DELAY); 
+
+   deployDrogue();
+   deployMain();
+   land();
 
    end();
+}
+
+/********************************************
+ * DEPLOY_DROGUE: Logic to decide when to 
+ *    deploy the drogue chute.
+ * ******************************************/
+void deployDrogue()
+{
+   // TODO: Write logic
+}
+
+/********************************************
+ * DEPLOY_MAIN: Logic to decide when to deploy
+ *    the main parachute.
+ * ******************************************/
+void deployMain()
+{
+   // TODO: Write logic
+}
+
+/********************************************
+ * LAND: Program to decide when the rocket has
+ *    landed and to stop collecting data. 
+ * ******************************************/
+void land()
+{
+   // TODO: Write logic
 }
